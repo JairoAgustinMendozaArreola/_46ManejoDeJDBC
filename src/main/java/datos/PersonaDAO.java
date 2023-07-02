a95c6cf246030a668a1bfad1b236be7e5503d70c
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-07-01 20:18:33 -0600

    180. Ejercicio: Manejo de JDBC - parte 4

--- src/main/java/datos/PersonaDAO.java
+++ src/main/java/datos/PersonaDAO.java
@@ -5,6 +5,8 @@ import domain.Persona;
 import java.sql.*;
 import java.util.*;
 
+import static datos.Conexion.*;
+
 public class PersonaDAO {
     private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM persona";
 
@@ -14,6 +16,33 @@ public class PersonaDAO {
         ResultSet rs = null;
         Persona persona = null;
         List<Persona> personas = new ArrayList<>();
+        try{
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                int idPersona = rs.getInt("id_persona");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido")
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+
+                persona = new Persona(idPersona,nombre,apellido,email,telefono);
+                personas.add(persona);
+            }
+
+        }catch (SQLException ex){
+            ex.printStackTrace();
+        }
+        finally {
+            try {
+                Conexion.close(rs);
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
