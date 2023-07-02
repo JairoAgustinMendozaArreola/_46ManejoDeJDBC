8b2fa417fafbd3be95cb51d0180d443a3779b7d6
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-07-01 19:56:45 -0600

    Reproducir 179. Ejercicio: Manejo de JDBC - parte 3 - MySql 8

---
+++ src/main/java/datos/PersonaDAO.java
@@ -0,0 +1,19 @@
+package datos;
+
+import domain.Persona;
+
+import java.sql.*;
+import java.util.*;
+
+public class PersonaDAO {
+    private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM persona";
+
+    public List<Persona> seleccionar(){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Persona persona = null;
+        List<Persona> personas = new ArrayList<>();
+
+    }
+}
